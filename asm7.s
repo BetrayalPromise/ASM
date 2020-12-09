2be6af2cf7af2b3a4fbe372c34066a8f88ff50ec
Yang <BetrayalPromise@gmail.com>
2020-12-09 14:12:22 +0800

    新增 macOS系统调用号封装

---
+++ asm7.s
@@ -0,0 +1,18 @@
+    .section __TEXT, __text
+    .globl _main
+
+_main:
+    movq $0x4, %rdi
+    callq _bsdsyscall
+    movq $0, %rax
+    retq
+
+# return mosOS system call number
+# uint bassyscall(uint number) 
+_bsdsyscall:
+    pushq %rbp
+    movq $0x0000000000000002, %rax
+    shlq $0x18, %rax
+    addq %rdi, %rax
+    popq %rbp
+    retq
