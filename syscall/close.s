49bc9de05fdd91c31f9d131a9fa4ea2502732c4f
Yang <BetrayalPromise@gmail.com>
2020-12-30 09:26:14 +0800

    整理 代码

--- syscall/close.s
+++ syscall/close.s
@@ -1,9 +1,10 @@
+    .section __TEXT, __text
     .global _main
-    p2align 4, 0x90
+    .p2align 4, 0x90
 
 _main:
     pushq   %rbp
     movq    %rsp, %rbp
-
+    xorq    %rax, %rax
     popq    %rbp
     retq
